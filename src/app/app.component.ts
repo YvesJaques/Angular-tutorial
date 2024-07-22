e15366a17ab5e8748942e407b6a29e5826888e21
Yves Jaques <yves.jaques@asksuite.com>
2024-07-22 20:35:51 -0300

    Defining component inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  users = DUMMY_USERS
 }
