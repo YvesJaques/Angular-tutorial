fad9071826c47f800741498d624920dee60b8ff4
Yves Jaques <yves.jaques@asksuite.com>
2024-09-18 23:04:36 -0300

    Using localStorage for Data Storage

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -28,6 +28,14 @@ export class TasksService {
         }
     ]
 
+    constructor() {
+        const tasks = localStorage.getItem('tasks');
+
+        if (tasks) {
+            this.tasks = JSON.parse(tasks)
+        }
+    }
+
     getUserTasks(userId: string) {
         return this.tasks.filter((task) => task.userId === userId)
     }
@@ -38,13 +46,20 @@ export class TasksService {
             userId,
             ...taskData
         })
+        this.saveTasks()
     }
 
     removeTask(id: string) {
         this.tasks = this.tasks.filter((task) => task.id !== id)
+        this.saveTasks()
     }
 
     completeTask(id: string) {
         this.tasks = this.tasks.filter((task) => task.id !== id)
+        this.saveTasks()
+    }
+
+    private saveTasks() {
+        localStorage.setItem('tasks', JSON.stringify(this.tasks))
     }
 }
